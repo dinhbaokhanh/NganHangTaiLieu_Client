472cec10b5ce600dc1c47c86b742b90af6c6aed4
dinhbaokhanh <khanhdbao@gmail.com>
2025-03-06 17:19:41 +0700

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Home = lazy(() => import('./pages/Home'))
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import React, { useState } from 'react'
+import AppLayout from '../components/layout/AppLayout'
+import DocumentTabs from '../components/layout/DocumentTabs'
+import documents from '../data/sampleDocuments'
+import defaultFileImg from '../assets/doc_image_default.png'
 
 const Home = () => {
-  return <div>Home</div>
+  const [activeTab, setActiveTab] = useState('theory')
+  const filteredDocs = documents.filter((doc) =>
+    activeTab === 'saved' ? doc.saved : doc.category === activeTab
+  )
+
+  return (
+    <div className="container mx-auto p-4">
+      <DocumentTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      <div className="mt-4 p-4 rounded-lg bg-white grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {filteredDocs.length > 0 ? (
+          filteredDocs.map((doc) => (
+            <div
+              key={doc.id}
+              className="relative border rounded-lg p-2 shadow-md"
+            >
+              <img
+                src={doc.thumbnail || defaultFileImg}
+                alt={doc.title}
+                className="w-full h-45 object-cover rounded-md"
+              />
+              <p className="flex items-center justify-center text-center font-medium mt-2 h-10">
+                {doc.title}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p className="text-gray-500">Không có tài liệu nào.</p>
+        )}
+      </div>
+    </div>
+  )
 }
 
-export default Home
+export default AppLayout(Home)
