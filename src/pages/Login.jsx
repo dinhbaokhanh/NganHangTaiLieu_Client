6dbf488b203381ee38f3087bbd9213afa2ea9dd5
dinhbaokhanh <khanhdbao@gmail.com>
2025-03-06 08:31:54 +0700

    login example

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return <div>Login</div>
+  const [isLogin, setIsLogin] = useState(true)
+
+  return (
+    <>
+      {isLogin ? (
+        <div>
+          <div>Login Form</div>
+          <button onClick={() => setIsLogin(!isLogin)}>
+            Switch to Register
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div>Register Form</div>
+          <button onClick={() => setIsLogin(!isLogin)}>Switch to Login</button>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Login
