ccb8730b447b12f2545c64ae86a380668f5bad11
dinhbaokhanh <khanhdbao@gmail.com>
2025-03-06 07:44:41 +0700

    Home & Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React, { Suspense } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+
+const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route>
-          {/* App Route */}
-          <Route path="/" />
-        </Route>
-        <Route path="login" />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route>
+            {/* App Route */}
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
