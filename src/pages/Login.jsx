908e23ef6d2e49407d0e4bd952c0bd8c0cc2ca5e
TuanNTT1412 <tuanntt1412@gmail.com>
2025-03-23 02:58:17 +0700

    Forgot Password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import LoginBg from "../assets/login_background.jpg";
 
 const Login = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [view, setView] = useState("login"); // "login", "register", "forgot", "reset"
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (view === "forgot") {
+      setView("reset");
+    }
+  };
 
   return (
     <div
@@ -14,12 +21,17 @@ const Login = () => {
           PTIT DOCUMENTS
         </h2>
         <h3 className="text-[#000000] text-center text-base sm:text-lg font-sans font-bold mt-2">
-          {isLogin ? "Đăng nhập" : "Đăng ký"}
+          {view === "forgot"
+            ? "Lấy lại mật khẩu"
+            : view === "reset"
+            ? "Lấy lại mật khẩu"
+            : view === "login"
+            ? "Đăng nhập"
+            : "Đăng ký"}
         </h3>
 
-        {/* Form */}
-        <form className="mt-4">
-          {!isLogin && (
+        <form className="mt-4" onSubmit={handleSubmit}>
+          {view === "register" && (
             <div className="mb-3 sm:mb-4">
               <input
                 type="email"
@@ -28,22 +40,28 @@ const Login = () => {
               />
             </div>
           )}
-          <div className="mb-3 sm:mb-4">
-            <input
-              type="text"
-              placeholder="Tên đăng nhập"
-              className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
-            />
-          </div>
-          <div className="mb-3 sm:mb-4">
-            <input
-              type="password"
-              placeholder="Mật khẩu"
-              className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
-            />
-          </div>
 
-          {!isLogin && (
+          {view !== "forgot" && view !== "reset" && (
+            <div className="mb-3 sm:mb-4">
+              <input
+                type="text"
+                placeholder="Tên đăng nhập"
+                className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
+              />
+            </div>
+          )}
+
+          {view !== "forgot" && view !== "reset" && (
+            <div className="mb-3 sm:mb-4">
+              <input
+                type="password"
+                placeholder="Mật khẩu"
+                className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
+              />
+            </div>
+          )}
+
+          {view === "register" && (
             <div className="mb-3 sm:mb-4">
               <input
                 type="password"
@@ -53,35 +71,71 @@ const Login = () => {
             </div>
           )}
 
-          {isLogin && (
+          {view === "forgot" && (
+            <div className="mb-3 sm:mb-4">
+              <input
+                type="email"
+                placeholder="Vui lòng nhập email của bạn"
+                className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
+              />
+            </div>
+          )}
+
+          {view === "reset" && (
+            <div className="mb-3 sm:mb-4">
+              <input
+                type="password"
+                placeholder="Vui lòng nhập password mới"
+                className="w-full px-3 py-2 font-sans border border-black bg-white text-black rounded-md"
+              />
+            </div>
+          )}
+
+          {view === "login" && (
             <div className="flex justify-between text-sm mb-4">
               <a
                 href="#"
                 className="text-black hover:text-red-600"
                 onClick={(e) => {
                   e.preventDefault();
-                  setIsLogin(false);
+                  setView("register");
                 }}
               >
                 Đăng ký
               </a>
-              <a href="#" className="text-black hover:text-red-600">
+              <a
+                href="#"
+                className="text-black hover:text-red-600"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setView("forgot");
+                }}
+              >
                 Quên mật khẩu
               </a>
             </div>
           )}
 
-          <button className="w-full bg-red-600 text-white py-2 font-semibold rounded-md border border-transparent hover:bg-white hover:text-red-600 hover:border hover:border-red-600 
-                   transition duration-200 cursor-pointer">
-            {isLogin ? "Đăng nhập" : "Đăng ký"}
+          <button
+            type="submit"
+            className="w-full bg-red-600 text-white py-2 font-semibold rounded-md border border-transparent hover:bg-white hover:text-red-600 hover:border hover:border-red-600 
+                   transition duration-200 cursor-pointer"
+          >
+            {view === "forgot"
+              ? "Nhập email"
+              : view === "reset"
+              ? "Nhập mật khẩu"
+              : view === "login"
+              ? "Đăng nhập"
+              : "Đăng ký"}
           </button>
         </form>
 
-        {!isLogin && (
+        {view !== "login" && (
           <p className="mt-3 text-center">
             <button
               className="text-black transition duration-200 hover:text-red-600 cursor-pointer"
-              onClick={() => setIsLogin(true)}
+              onClick={() => setView("login")}
             >
               Về trang đăng nhập
             </button>
