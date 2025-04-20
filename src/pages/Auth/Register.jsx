0f213f2889203d98bbc8ea28975a45b97be45034
TuanNTT1412 <tuanntt1412@gmail.com>
2025-04-20 22:27:54 +0700

    Update Auth FE

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,41 +1,65 @@
+import { useState } from 'react'
+import { FaEnvelope, FaUser, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa'
+
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
   return (
     <>
       <h3 className="text-black text-center text-lg font-sans font-bold mt-2">
         Đăng ký
       </h3>
       <form className="mt-4">
-        <div className="mb-3 sm:mb-4">
+        <div className="mb-3 sm:mb-4 relative">
+          <FaEnvelope className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
             type="email"
             placeholder="Email"
-            className="w-full px-3 py-2 border border-black bg-white text-black rounded-md"
+            className="w-full pl-10 px-3 py-2 border border-black bg-white text-black rounded-md"
           />
         </div>
-        <div className="mb-3 sm:mb-4">
+        <div className="mb-3 sm:mb-4 relative">
+          <FaUser className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
             type="text"
             placeholder="Tên đăng nhập"
-            className="w-full px-3 py-2 border border-black bg-white text-black rounded-md"
+            className="w-full pl-10 px-3 py-2 border border-black bg-white text-black rounded-md"
           />
         </div>
-        <div className="mb-3 sm:mb-4">
+        <div className="mb-3 sm:mb-4 relative">
+          <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             placeholder="Mật khẩu"
-            className="w-full px-3 py-2 border border-black bg-white text-black rounded-md"
+            className="w-full pl-10 px-3 py-2 border border-black bg-white text-black rounded-md"
           />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+          >
+            {showPassword ? <FaEye /> : <FaEyeSlash />}
+          </button>
         </div>
-        <div className="mb-3 sm:mb-4">
+        <div className="mb-3 sm:mb-4 relative">
+          <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
-            type="password"
+            type={showConfirmPassword ? 'text' : 'password'}
             placeholder="Xác nhận mật khẩu"
-            className="w-full px-3 py-2 border border-black bg-white text-black rounded-md"
+            className="w-full pl-10 px-3 py-2 border border-black bg-white text-black rounded-md"
           />
+          <button
+            type="button"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+          >
+            {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
+          </button>
         </div>
         <button
           type="submit"
-          className="w-full bg-red-600 text-white py-2 font-semibold rounded-md hover:bg-white hover:text-red-600 border border-transparent hover:border-red-600 transition duration-200"
+          className="w-full bg-red-600 text-white py-2 font-semibold rounded-md hover:cursor-pointer hover:bg-white hover:text-red-600 border border-transparent hover:border-red-600 transition duration-200"
         >
           Đăng ký
         </button>
--- src/pages/Auth/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword.jsx
@@ -1,20 +1,33 @@
+import { useState } from 'react'
+import { FaLock, FaEye, FaEyeSlash } from 'react-icons/fa'
+
 const Reset = () => {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <>
       <h3 className="text-black text-center text-lg font-sans font-bold mt-2">
         Đặt lại mật khẩu
       </h3>
       <form className="mt-4">
-        <div className="mb-3 sm:mb-4">
+        <div className="mb-3 sm:mb-4 relative">
+          <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             placeholder="Vui lòng nhập mật khẩu mới"
-            className="w-full px-3 py-2 border border-black bg-white text-black rounded-md"
+            className="w-full pl-10 px-3 py-2 border border-black bg-white text-black rounded-md"
           />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500  hover:cursor-pointer"
+          >
+            {showPassword ? <FaEye/> : <FaEyeSlash />}
+          </button>
         </div>
         <button
           type="submit"
-          className="w-full bg-red-600 text-white py-2 font-semibold rounded-md hover:bg-white hover:text-red-600 border border-transparent hover:border-red-600 transition duration-200"
+          className="w-full bg-red-600 text-white py-2 font-semibold rounded-md hover:cursor-pointer hover:bg-white hover:text-red-600 border border-transparent hover:border-red-600 transition duration-200"
         >
           Đặt lại mật khẩu
         </button>
